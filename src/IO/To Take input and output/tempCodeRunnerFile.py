5356ff358ee8b171b2d3a34b2bfffb2c1cd8e4fa
GauravNautiyal <gn.nautiyalgaurav@gmail.com>
2020-10-21 18:38:47 +0530

    String code

--- src/IO/To Take input and output/tempCodeRunnerFile.py
+++ src/IO/To Take input and output/tempCodeRunnerFile.py
@@ -1,3 +1,9 @@
+def Replace(str1): 
+    str1 = str1.replace(', ', 'third') 
+    str1 = str1.replace('.', ', ') 
+    str1 = str1.replace('third', '.') 
+    return str1 
+      
+str1 = "14, 625, 498.002"
+print(Replace(str1))  
 
-
-print("\n double quotes ")
